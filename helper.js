4510f73e31749d9f01d0abf742af5ebd7be59611
Winstonjie <jiewandana888@gmail.com>
2025-05-05 11:33:19 +0700

    tetsting

---
+++ helper.js
@@ -0,0 +1,60 @@
+// Vector and Matrix operations using gl-matrix
+const vec3 = glMatrix.vec3;
+const mat4 = glMatrix.mat4;
+
+// Vector operations
+function add(a, b) {
+    return vec3.add(vec3.create(), a, b);
+}
+
+function subtract(a, b) {
+    return vec3.subtract(vec3.create(), a, b);
+}
+
+function scale(v, s) {
+    return vec3.scale(vec3.create(), v, s);
+}
+
+function normalize(v) {
+    return vec3.normalize(vec3.create(), v);
+}
+
+function dot(a, b) {
+    return vec3.dot(a, b);
+}
+
+function length(v) {
+    return vec3.length(v);
+}
+
+// Ray structure
+class Ray {
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+}
+
+// Sphere structure
+class Sphere {
+    constructor(center, radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+}
+
+// Ray-Sphere intersection
+function intersectRaySphere(ray, sphere) {
+    const oc = subtract(ray.origin, sphere.center);
+    const a = dot(ray.direction, ray.direction);
+    const b = 2.0 * dot(oc, ray.direction);
+    const c = dot(oc, oc) - sphere.radius * sphere.radius;
+    const discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1; // No intersection
+    }
+
+    const t = (-b - Math.sqrt(discriminant)) / (2.0 * a);
+    return t;
+} 
